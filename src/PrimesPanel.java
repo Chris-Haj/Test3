f88851eab5f8dd9521a6eb3a1ff09e2309c0d4cf
Chris-Haj <chrishaj1191@gmail.com>
2021-06-21 02:15:31 +0300

    push 3

--- src/PrimesPanel.java
+++ src/PrimesPanel.java
@@ -2,14 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+/*
+    @author Christopher Haj
 
+    this class will use the JPanel to create and draw Buttons, Textfields , and Labels
+    that will be used to take in numbers of the user and after taking receiving an input
+    it will be able to draw a table of blue and white boxes depending on prime numbers.
+ */
 public class PrimesPanel extends JPanel {
 
     private JButton Calculate, Clear;
     private JTextField Numbertxt, Colstxt;
     private JLabel numlbl, colslbl;
     private int Cols, boxes, boxheight, buttonsBG, BoxX, BoxY, xCheck, drawCols, drawBox;
-    private String warning="",ColsString="",BoxesString="";
+    private String warning = "", ColsString = "", BoxesString = "";
 
 
     public PrimesPanel() {
@@ -40,57 +46,74 @@ public class PrimesPanel extends JPanel {
 
         @Override
         public void actionPerformed(ActionEvent e) throws NumberFormatException {
+            /*
+                in the Numbertxt and Colstxt textfields, anything that is passed into them
+                will go though a try block to check if what was passed in is a positive number or not
+                and if it is not it will intialize the warning String to "Must input a positive number!"
+                and write it on the screen.
+             */
             if (e.getSource() == Numbertxt) {
                 try {
-                    if(Integer.parseInt(Numbertxt.getText())<1) {
+                    if (Integer.parseInt(Numbertxt.getText()) < 1) {
                         warning = "Must input a positive number!";
                         repaint();
-                    }
-                    else {
+                    } else {
                         boxes = Integer.parseInt(Numbertxt.getText());
-                        BoxesString="Number of boxes to draw: "+Numbertxt.getText();
-                        warning="";
+                        BoxesString = "Number of boxes to draw: " + Numbertxt.getText();
+                        warning = "";
                         repaint();
                     }
                     Numbertxt.setText("");
 
                 } catch (NumberFormatException a) {
                     Numbertxt.setText("");
-                    warning="Must input a positive number!";
+                    warning = "Must input a positive number!";
                     repaint();
                 }
             } else if (e.getSource() == Colstxt) {
                 try {
-                    if(Integer.parseInt(Colstxt.getText())<1) {
+                    if (Integer.parseInt(Colstxt.getText()) < 1) {
                         warning = "Must input a positive number!";
                         repaint();
-                    }
-                    else {
+                    } else {
                         Cols = Integer.parseInt(Colstxt.getText());
                         warning = "";
-                        ColsString="Number of columns to draw: "+Colstxt.getText();
+                        ColsString = "Number of columns to draw: " + Colstxt.getText();
                         repaint();
                     }
                     Colstxt.setText("");
 
                 } catch (NumberFormatException a) {
                     Colstxt.setText("");
-                    warning="Must input a positive number!";
+                    warning = "Must input a positive number!";
                     repaint();
                 }
+            /*
+                When the Calculate button is pressed it will use the numbers passed in
+                to draw a table of blue and white boxes.
+                This will also show on the left side of the menu at the top how many
+                boxes and columns were drawn.
+             */
             } else if (e.getSource() == Calculate) {
-                drawBox=boxes;
-                drawCols=Cols;
-                BoxesString="Boxes drawn: "+boxes;
-                ColsString="Columns drawn: "+Cols;
+                drawBox = boxes;
+                drawCols = Cols;
+                BoxesString = "Boxes drawn: " + boxes;
+                ColsString = "Columns drawn: " + Cols;
                 repaint();
+            /*
+                If the clear button is pressed it will reset all Integer variables that are
+                related to the number of boxes and columns to 0.
+                And it also resets the Strings related to them.
+
+             */
             } else if (e.getSource() == Clear) {
                 Cols = 0;
                 boxes = 0;
-                drawCols=0;
-                drawBox=0;
-                BoxesString="";
-                ColsString="";
+                drawCols = 0;
+                drawBox = 0;
+                BoxesString = "";
+                ColsString = "";
+                warning="";
                 repaint();
 
             }
@@ -101,35 +124,40 @@ public class PrimesPanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        boxheight = getHeight()/15;
+        boxheight = getHeight() / 15;
         BoxX = 0;
         BoxY = buttonsBG;
         buttonsBG = getHeight() / 16;
         xCheck = 1;
 
-
-
-
+/*
+    this is used to draw a background for the menu on the top of the screen with
+    a extra messages popping up displaying the number of boxes, and columns drawn.
+    It also requests the user to pass in a positive number, and displays a
+    warning if the user has entered a String or a negative number
+ */
         g.setColor(Color.gray);
         g.fillRect(0, 0, getWidth(), buttonsBG);
         g.setColor(Color.black);
-        g.drawString("Please press enter after passing a number!",(getWidth()/5)*2,getHeight()/23);
-        g.drawString(warning,(getWidth()/5)*2+50,getHeight()/18);
-        g.drawString(BoxesString,getWidth()/40,getHeight()/33);
-        g.drawString(ColsString,getWidth()/40,getHeight()/23);
-
+        g.drawString("Please press enter after passing a number!", (getWidth() / 5) * 2, getHeight() / 23);
+        g.drawString(warning, (getWidth() / 5) * 2 + 50, getHeight() / 18);
+        g.drawString(BoxesString, getWidth() / 40, getHeight() / 33);
+        g.drawString(ColsString, getWidth() / 40, getHeight() / 23);
+/*
+    this for loop is to draw each square seperately and calls the isPrime function to check
+    if the box color should be blue if the box number is a prime and white if not.
+ */
         for (int i = 1; i <= drawBox; i++) {
             if (isPrime(i) == true) {
                 g.setColor(Color.cyan);
-            }
-            else
+            } else
                 g.setColor(Color.white);
             g.fillRect(BoxX, BoxY, getWidth() / drawCols, boxheight);
             g.setColor(Color.black);
             g.drawRect(BoxX, BoxY, getWidth() / drawCols, boxheight);
-            if (xCheck ==drawCols) {
+            if (xCheck == drawCols) {
                 BoxX = 0;
-                xCheck=0;
+                xCheck = 0;
                 BoxY += boxheight;
             } else
                 BoxX += getWidth() / drawCols;
@@ -138,7 +166,9 @@ public class PrimesPanel extends JPanel {
 
         }
     }
-
+/*
+    private method to check if the box number is a prime number
+ */
     private static boolean isPrime(int n) {
         if (n <= 1)
             return false;
